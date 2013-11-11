f5cf75897bc4cea8513449ee7589e5baabcfab82
Maxim Ponomarev <maxpon@yandex-team.ru>
2013-11-12 03:27:17 +0600

    Big update
    - add i10n
    - add format tokens
    - update custom formats

---
+++ lang/tr.js
@@ -0,0 +1,23 @@
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['datef'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('../datef'));
+    } else {
+        factory(window.datef);
+    }
+}(function (datef) {
+    datef.lang('tr', {
+        _months: 'Ocak_Şubat_Mart_Nisan_Mayıs_Haziran_Temmuz_Ağustos_Eylül_Ekim_Kasım_Aralık'.split('_'),
+        months: function (date) {
+            return this._months[date.getMonth()];
+        },
+        _monthsShort: 'Oca_Şub_Mar_Nis_May_Haz_Tem_Ağu_Eyl_Eki_Kas_Ara'.split('_'),
+        monthsShort: function (date) {
+            return this._monthsShort[date.getMonth()];
+        },
+        weekdays: 'Pazar_Pazartesi_Salı_Çarşamba_Perşembe_Cuma_Cumartesi'.split('_'),
+        weekdaysShort: 'Paz_Pts_Sal_Çar_Per_Cum_Cts'.split('_'),
+        weekdaysMin: 'Pz_Pt_Sa_Ça_Pe_Cu_Ct'.split('_')
+    });
+}));
